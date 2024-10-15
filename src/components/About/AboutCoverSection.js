14a26ce42b3d0ed02cba20473beba40a1e1edbc1
thanhdicode <thanhhdse184021@fpt.edu.vn>
2024-10-15 15:05:52 +0700

    Done !

---
+++ src/components/About/AboutCoverSection.js
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import profileCharacter from "../../../public/character.png"
+
+const AboutCoverSection = () => {
+  return (
+    <section className='w-full md:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex flex-col md:flex-row items-center justify-center text-dark dark:text-light'>
+        <div className='w-full md:w-1/2 h-full border-r-2 border-solid border-dark dark:border-light flex justify-center'> 
+            <Image src={profileCharacter} alt="CodeBucks" 
+            className='w-4/5  xs:w-3/4 md:w-full h-full object-contain object-center'
+            priority
+            sizes="(max-width: 768px) 100vw,(max-width: 1180px) 50vw, 50vw"
+            />
+        </div>
+
+        <div className='w-full md:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:p-10 pb-10 lg:px-16'>
+            <h2 className='font-bold capitalize text-4xl xs:text-5xl sxl:text-6xl  text-center lg:text-left'>
+            Dream Big, Work Hard, Achieve More!
+            </h2>
+            <p className='font-medium capitalize mt-4 text-base'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quod.
+            </p>
+        </div>
+    </section>
+  )
+}
+
+export default AboutCoverSection
---
+++ src/components/Blog/BlogDetails.js
@@ -0,0 +1,23 @@
+import { format, parseISO } from "date-fns";
+import { slug } from "github-slugger";
+import Link from "next/link";
+
+const BlogDetails = ({ blog, slug: blogSlug }) => {
+  return (
+    <div className="px-2  md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5  md:mx-10 rounded-lg">
+      <time className="m-3">
+        {format(parseISO(blog.publishedAt), "LLLL d, yyyy")}
+      </time>
+      <span className="m-3">
+        {/* <ViewCounter slug={blogSlug} /> */}
+        100,000+ views
+      </span>
+      <div className="m-3">{blog.readingTime.text}</div>
+      <Link href={`/categories/${slug(blog.tags[0])}`} className="m-3">
+        #{blog.tags[0]}
+      </Link>
+    </div>
+  );
+};
+
+export default BlogDetails;
